58871ecf53b5e22d4b6a0b0d6622b0853d8aaf15
Omkar Pathak <omkarpathak27@gmail.com>
2025-12-28 20:07:03 +0530

    added admin options

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Expense
 
-# Register your models here.
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'description', 'category', 'amount', 'user')
+    list_filter = ('category', 'date', 'user')
+    search_fields = ('description', 'category')
+    ordering = ('-date',)
